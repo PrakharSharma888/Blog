7f847cd5dab0a0281c0510c5810d63fb95443932
Prakhar <prakharentp2002@gmail.com>
2021-06-28 17:40:33 +0530

    Newest

--- Verylatest/settings.py
+++ Verylatest/settings.py
@@ -81,11 +81,8 @@ TEMPLATES = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'accounts',
-        'USER': 'postgres',
-        'PASSWORD': 'Bhanusharma001',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
