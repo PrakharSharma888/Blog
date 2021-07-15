fd71f037409e9eab3ad659196c695ffca95f4c17
Prakhar <prakharentp2002@gmail.com>
2021-07-15 17:50:24 +0530

    done

--- users/signals.py
+++ users/signals.py
@@ -2,12 +2,17 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from .models import Profile
 from django.dispatch import receiver
+from django.core.exceptions import ObjectDoesNotExist
 
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
-    if created:
+    user = instance
+    try:
+        instance.profile.save()
+    except ObjectDoesNotExist:
         Profile.objects.create(user=instance)
+
 
 @receiver(post_save, sender=User)
 def save_user(sender, instance, **kwargs):
